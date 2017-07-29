a3aa5fc8dae13fb8729ba47361bb7a33aba9f84d
Andrew Wang <andrewwang@Andrews-MacBook-Air.local>
2017-07-28 22:33:14 -0400

    Refactor 'Transform'

--- 1.2/transform/main.cpp
+++ 1.2/transform/main.cpp
@@ -8,6 +8,47 @@ LANG: C++
 #include <iostream>
 using namespace std;
 
+/// function declarations
+bool isSame(char** bt, char** at, int sideLength);
+char** reflectGrid(char **bt, int sideLength);
+char** rotateGrid(char** bt, int sideLength);
+int transformation(char** bt, char** at, int sideLength);
+
+int main() { 
+
+	/* overhead initializations and inputs*/
+	ifstream fin("transform.in");
+	ofstream fout("transform.out");
+
+	int N; 
+	fin >> N;
+
+	char** bt = new char*[10]; // before transformation (space of 10 required)
+	char** at = new char*[10]; // after transformation
+
+	for (int i = 0; i < 10; i++) { 
+		bt[i] = new char[10];
+		at[i] = new char[10];
+	}
+
+	/* accept input */
+	for (int i = 0; i < N; i++) { 
+		for (int j = 0; j < N; j++) {
+			fin >> bt[i][j]; 
+		}
+	}
+
+	for (int i = 0; i < N; i++) { 
+		for (int j = 0; j < N; j++) {
+			fin >> at[i][j]; 
+		}
+	}
+
+	fout << transformation(bt, at, N) << "\n";
+
+	return 0;
+}
+
 bool isSame(char** bt, char** at, int sideLength) {
 	// determine whether two char grids, a and b, are the same.
 	for (int i = 0; i < sideLength; i++) { 
@@ -19,6 +60,7 @@ bool isSame(char** bt, char** at, int sideLength) {
 	return true;
 }
 
+/// function definitions
 char** reflectGrid(char **bt, int sideLength) { 
 	char** reflectedGrid = new char*[sideLength]; 
 	
@@ -55,59 +97,23 @@ char** rotateGrid(char** bt, int sideLength) {
 int transformation(char** bt, char** at, int sideLength) { 
 // determine which transformation (1 - 6), which when applied
 // to bt, yields at. If none, return 7.
-	int transformationID = 7;
-	if (isSame(bt, at, sideLength)) transformationID = 6;
 	if (isSame(rotateGrid(bt, sideLength), at, sideLength)) {
-		transformationID = 1; // 90 deg
+		return 1;// 90 deg
 	}
 	if (isSame(rotateGrid(rotateGrid(bt, sideLength), sideLength), at, sideLength)) {
-	 	if (2 < transformationID) transformationID = 2; // 90 + 90 deg
+	 	return 2; // 90 + 90 deg
 	}
 	if (isSame(rotateGrid(rotateGrid(rotateGrid(bt, sideLength), sideLength), sideLength), at, sideLength)) {
-		if (3 < transformationID) transformationID = 3;
+		return 3;
 	}
 	if (isSame(reflectGrid(bt, sideLength), at, sideLength)) {
-		if (4 < transformationID) transformationID = 4; // reflect
+		return 4;// reflect
 	}
 	if (isSame(rotateGrid(reflectGrid(bt, sideLength), sideLength), at, sideLength) ||
 			 isSame(rotateGrid(rotateGrid(reflectGrid(bt, sideLength), sideLength), sideLength), at, sideLength) ||
 			 isSame(rotateGrid(rotateGrid(rotateGrid(reflectGrid(bt, sideLength), sideLength), sideLength), sideLength), at, sideLength)){
-		if (5 < transformationID) transformationID = 5;
+		return 5;
 	} 
-	return transformationID;
-}
-
-int main() { 
-
-	/* overhead initializations and inputs*/
-	ifstream fin("transform.in");
-	ofstream fout("transform.out");
-
-	int N; 
-	fin >> N;
-
-	char** bt = new char*[10]; // before transformation (space of 10 required)
-	char** at = new char*[10]; // after transformation
-
-	for (int i = 0; i < 10; i++) { 
-		bt[i] = new char[10];
-		at[i] = new char[10];
-	}
-
-	/* accept input */
-	for (int i = 0; i < N; i++) { 
-		for (int j = 0; j < N; j++) {
-			fin >> bt[i][j]; 
-		}
-	}
-
-	for (int i = 0; i < N; i++) { 
-		for (int j = 0; j < N; j++) {
-			fin >> at[i][j]; 
-		}
-	}
-
-	fout << transformation(bt, at, N) << "\n";
-
-	return 0;
+	if (isSame(bt, at, sideLength)) return 6;
+	else return 7;
 }
