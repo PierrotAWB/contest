5f7ff6d101991bff7eb4ef0690631f5725e9b4fd
PierrotAWB <andrewwang298@gmail.com>
2019-05-18 18:36:33 -0400

    Complete Stamps

--- USACO1/Chapter 3/3.1/Stamps/main.cpp
+++ USACO1/Chapter 3/3.1/Stamps/main.cpp
@@ -5,60 +5,34 @@ LANG: C++11
 */
 
 #include <algorithm>
-#include <array>
-#include <bitset>
 #include <cstdio>
-#include <deque>
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <map> 
-#include <math.h>
-#include <queue>
 #include <stdlib.h>
-#include <set>
-#include <string>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
 FILE *fin = fopen("stamps.in", "r");
 FILE *fout = fopen("stamps.out", "w");
 
-int K, N, M, a[51];
-bool b[2000001];
+int K, N, i, a[51];
+
+// b[i] stores the LEAST number of stamps whose combined value is i.
+int b[2000001];
 
 int main()
 {
-	int maxStamp = -1;
 	fscanf(fin, "%d %d ", &K, &N);
-	for (int i = 0; i < N; ++i) {
+	for (i = 0; i < N; ++i) {
 		fscanf(fin, "%d ", &a[i]);
-		maxStamp = max(maxStamp, a[i]);
 	}
-	M = K * maxStamp; // upper bound for answer.
 
-	int d = 1;
-	for (int i = 0; i < N; ++i) b[a[i]] = true;
-	
-	for (d = 2; d <= K; ++d) {
-		vector<int> toUpdate;
-		for (int i = 1; i < d * maxStamp; ++i) {
-			if (b[i] == true) {
-				for (int j = 0; j < N; ++j) {
-					if (b[i + a[j]] == false) toUpdate.push_back(i + a[j]);
-				}	
-			}
+	for (i = 1; i <= 10000*K; ++i) {
+		b[i] = K+1;	
+		for (int j = 0; j < N; ++j) {
+			if (i - a[j] >= 0) b[i] = min(b[i], b[i - a[j]] + 1);
 		}
-		for (auto u : toUpdate)
-			if (u <= M) 
-				b[u] = true;
+		if (b[i] > K) break;
 	}
 
-	int ans = 1;
-	while (b[ans]) ++ans;
-	fprintf(fout, "%d\n", ans - 1);
-
+	fprintf(fout, "%d\n", i - 1);
     return 0;
 }
