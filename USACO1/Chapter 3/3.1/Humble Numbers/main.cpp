c5b091765daa118d26508d384522355b5f22d1cd
PierrotAWB <andrewwang298@gmail.com>
2019-05-18 14:00:48 -0400

    Complete Humble numbers

--- USACO1/Chapter 3/3.1/Humble Numbers/main.cpp
+++ USACO1/Chapter 3/3.1/Humble Numbers/main.cpp
@@ -4,40 +4,39 @@ PROG: humble
 LANG: C++11
 */
 
-#include <algorithm>
-#include <array>
-#include <bitset>
 #include <cstdio>
-#include <deque>
-#include <fstream>
-#include <iostream>
-#include <iomanip>
-#include <map> 
-#include <math.h>
-#include <queue>
 #include <stdlib.h>
 #include <set>
-#include <string>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
 FILE *fin = fopen("humble.in", "r");
 FILE *fout = fopen("humble.out", "w");
 
-int K, N;
-map<int, int> m;
+int K, N, num[101];
+set<int> s;
 
 int main()
 {
     fscanf(fin , "%d %d ", &K, &N);
     for (int i = 0; i < K; ++i) {
-        int p;
-        fscanf(fin , "%d", &p);
-
+        fscanf(fin , "%d", &num[i]);
+        s.insert(num[i]);
+    }   
+    // Essentially, we maintain a set of size N, replacing the last element
+    // until we can no more.
+    for (int i = 0; i < K; ++i) {
+        auto it = s.begin();
+        while (1) {
+            int t = (*it)*num[i];
+            if (t < 0) break;
+            if (s.size() > N) {
+                s.erase(--s.end());
+                if (t > *(--s.end())) break;
+            }
+            s.insert(t);
+            ++it;
+        }
     }
-
-	for (auto u : ans) fprintf(fout, "%lld\n", u);
-    return 0;
+    fprintf(fout, "%d\n", *(--s.end()));
 }
